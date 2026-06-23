7dca07bfac8b835c9aa698b62d0b0a0b5c086a4d
Shantanu Bhasme <shantanubhasme@Shantanus-Mac.local>
2026-06-23 13:00:13 +0530

    Implemented data driven testing for checkout flow

--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -5,7 +5,72 @@ import { loginData } from '../test-data/loginData';
 import { environment } from '../config/environment';
 import { checkoutData } from '../test-data/checkoutData';
 
-test.beforeEach(async ({ page }) => 
+
+for (const user of loginData){
+
+    test(`Cart Purchase Flow - ${user.username}`,async ({ page, productsPage, cartPage, checkoutPage, 
+    overviewPage, completePage  })=>{
+
+        const loginPage = new LoginPage(page); 
+
+        await page.goto(environment.baseURL);
+
+        await loginPage.login(
+            user.username,
+            user.password
+        );
+
+    // Business flow starts here
+    
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+
+    await productsPage.verifyProductsPage();
+
+    await productsPage.addToCart();
+
+    await productsPage.openCart();
+
+    await cartPage.verifyBackpackInCart();
+
+    //await cartPage.removeProduct();
+
+    //await cartPage.verifyProductRemoved();
+
+    await cartPage.verifyProductDescription();
+
+    await cartPage.verifyProductPrice();
+
+    await cartPage.verifycontinueShoppingButton();
+
+    await cartPage.clickCheckoutButton();
+
+    await checkoutPage.verifyPageTitle();
+
+    await checkoutPage.fillUserInformation(
+        checkoutData.firstName,
+        checkoutData.lastName,
+        checkoutData.postalCode
+    );
+
+    await checkoutPage.clickContinue();
+
+    await overviewPage.verifyOverviewPage();
+
+    await overviewPage.verifyPaymentInformation();
+
+    await overviewPage.verifyShippingInformation();
+
+    await overviewPage.verifyTotalPrice();
+
+    await overviewPage.clickFinish();
+
+    await completePage.verifyPageTitle();
+
+    await completePage.verifyOrderSuccessMessage();
+    });
+}
+
+/*test.beforeEach(async ({ page }) => 
 {
     const loginPage = new LoginPage(page); 
 
@@ -71,4 +136,4 @@ test('@smoke Complete Purchase Flow', async ({ productsPage, cartPage, checkoutP
     await completePage.verifyPageTitle();
 
     await completePage.verifyOrderSuccessMessage();
-});
+}); */
